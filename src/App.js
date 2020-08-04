0ac526651fc145b45965ce0f94c55e9920b4dc25
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-08-04 18:26:58 +0430

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { Component } from 'react';
+import AddData from './abis/AddData.json'
 
-function App() {
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    const myaccount=accounts[0]
+    this.setState({ account:  myaccount })
+    const networkId = await web3.eth.net.getId()
+    const networkData = AddData.networks[networkId]
+    if(networkData) {
+      const adddata = web3.eth.Contract(AddData.abi, networkData.address)
+      this.setState({ adddata })
+      const dataCount = await adddata.methods.dataCount().call()
+      this.setState({ dataCount })
+      // Load datas
+      for (var i = 1; i <= dataCount; i++) {
+        const data = await adddata.methods.datas(i).call()
+        this.setState({
+          datas: [...this.state.datas, data]
+        })
+      }
+      this.setState({ loading: false})
+    } else {
+      window.alert('The contract not deployed to detected network.')
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      dataCount: 0,
+      datas: [],
+      loading: true
+    }
+
+    this.createData = this.createData.bind(this)
+   
+  }
+
+  createData(jsonvalue, identity) {
+    this.setState({ loading: true })
+    this.state.adddata.methods.createData(jsonvalue, identity).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +94,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
